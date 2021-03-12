781067b5e3adb95153fdc98bf9495286d8cd9082
Deepali Bamble <bambledeepali@gmail.com>
2021-03-12 17:46:18 +0530

    Feat:Added two cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+        extend: {
+          colors: {
+            'primary': '#667eea',
+          }
+        }
+
   },
   variants: {
     extend: {},
